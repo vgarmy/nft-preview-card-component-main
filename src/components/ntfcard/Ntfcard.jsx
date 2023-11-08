05e664064b517230f643a0844d61dc03f7c8fb62
Vladimir Gatara <vladimir.gatara@carbiotix.com>
2023-11-08 22:14:32 +0100

    Fixes hover

--- src/components/ntfcard/Ntfcard.jsx
+++ src/components/ntfcard/Ntfcard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ntfcard.css';
 import Equilibrium from '../../images/image-equilibrium.jpg';
+import EyeIcon from '../../images/icon-view.svg'
 import Clock from '../../images/icon-clock.svg';
 import Etherum from '../../images/icon-ethereum.svg';
 import Avatar from '../../images/image-avatar.png';
@@ -9,21 +10,24 @@ import Avatar from '../../images/image-avatar.png';
 const Ntfcard = () => {
   return (
     <div className='container' role="main">
-            <div className='card__container'>
-              <div className='img_container'>
-                <img src={Equilibrium} alt="equilibrium" className='icon-ethereum'/>
-              </div>
-                <h2>Equilibrium #3429</h2>
-                <p className='card__container_text'>Our Equilibrium collection promotes balance and calm.</p>
-                <div className='card__container_information'>
-                    <p className='clock-text'><img src={Etherum} alt="Etherum"/> 0.041 ETH</p>
-                    <p className='etherum-text'><img src={Clock} alt="Clock"/> 3 days left</p>
-                </div>
-                <hr />
-                <div className='avatar_text'>
-                  <img src={Avatar} alt="Avatar"/> <p>Creation of</p>&nbsp;<p className='avatar_name'>Jules Wyvern</p>
-                </div>
-            </div>
+      <div className='card__container'>
+        <div className='img_container'>
+          <img src={Equilibrium} alt="equilibrium" />
+          <div className='img_container_hover'>
+            <img src={EyeIcon} alt="eye icon" />
+          </div>
+        </div>
+        <h2>Equilibrium #3429</h2>
+        <p className='card__container_text'>Our Equilibrium collection promotes balance and calm.</p>
+        <div className='card__container_information'>
+          <p className='clock-text'><img src={Etherum} alt="Etherum" /> 0.041 ETH</p>
+          <p className='etherum-text'><img src={Clock} alt="Clock" /> 3 days left</p>
+        </div>
+        <hr />
+        <div className='avatar_text'>
+          <img src={Avatar} alt="Avatar" /> <p>Creation of</p>&nbsp;<p className='avatar_name'>Jules Wyvern</p>
+        </div>
+      </div>
     </div>
   )
 }
