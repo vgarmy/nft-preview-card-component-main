bce558160af791973861281914fd6b490fbde143
Vladimir Gatara <vladimir.gatara@carbiotix.com>
2023-11-08 15:33:14 +0100

    Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './components/footer/Footer';
 import Ntfcard from './components/ntfcard/Ntfcard';
 
 function App() {
   return (
     <div className="App">
         <Ntfcard />
+        <Footer />
     </div>
   );
 }
